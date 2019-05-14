5a234ff1e2635da92b5f96c93c7d06879e28ed37
gavin-boyd <g.boyd@ulster.ac.uk>
2019-05-14 12:27:35 +0100

    latest stable build

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,11 +12,10 @@ require('foundation-sites');
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
-
-
 $(document).foundation();
 
 //slick slider
+require('slick-carousel');
 jQuery(document).ready(function() {
   jQuery('.uls-slick').each(function() {
     jQuery(this).slick({
